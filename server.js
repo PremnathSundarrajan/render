1d1760c7480ad2427a33157a88df723c3c9d5bb0
PremnathSundarrajan <premsundarrajan4@gmail.com>
2025-11-25 18:54:37 +0530

    updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-
+let change;
 // Parse JSON body
 app.use(express.json());
 app.get('/bot',(req,res)=>{
@@ -10,21 +10,24 @@ app.get('/bot',(req,res)=>{
 
 app.post('/explore',(req,res)=>{
     res.status(200).send("Great choice! 🌆 Here are the available parking locations you can explore. Each place includes details like total slots, nearby landmarks, and availability to help you choose the perfect spot. Just select a location to see more information or book a slot instantly!")
+    //parking area cities will be suggested here
 })
 
 app.post('/explore/area',(req,res)=>{
     body = req.body;
     console.log(body);
-    res.status(200).send(`${body.message} has 20 parking areas`);
+    //city name varum, then we need to suggests parking areas of chennai here
+    res.status(200).send(`${body.place} has 20 parking areas`);
 })
 app.post("/salesiq/webhook", (req, res) => {
     const body = req.body;
+
 
     if(!body || !body.message){
         return res.status(400).json({ text: "Invalid request" });
     }
 
-    // Parse the inner JSON string
+    
     let msgObj;
     try {
         msgObj = JSON.parse(body.message);
@@ -32,7 +35,6 @@ app.post("/salesiq/webhook", (req, res) => {
         return res.status(400).json({ text: "Invalid message format" });
     }
 
-    // Now you can access the text
     const userText = msgObj.text;
 
     if(userText === "Fever"){
@@ -42,16 +44,38 @@ app.post("/salesiq/webhook", (req, res) => {
     }
 });
 
-app.post('/explore/area/book', (req,res)=>{
+app.post('/explore/area/tambaram', (req,res)=>{
     const body = req.body;
+    // name of parking area will come and we will suggests the book a slot here
     console.log(body);
-    if(body.place && body.book_a_slot_here){
-        res.status(200).send("Booked successfully")
+    if(body.area){
+        change= "Tambaram";
+        res.status(200).send("Can I book a slot here for you")
     }else{
         res.status(404).send("Please mention area");
     }
 })
 
+app.post('/explore/area/chrompet', (req,res)=>{
+    const body = req.body;
+    // name of parking area will come and we will suggests the book a slot here
+    console.log(body);
+    if(body.area){
+        change= "Chrompet";
+        res.status(200).send("Booked successfully")
+    }else{
+        res.status(404).send("Please mention area");
+    }
+});
+app.post('/explore/area/book',(req,res)=>{
+    if(change == "Chrompet"){
+        res.status(200).send("Booked successfully");
+    }else if (change == "Tambaram"){
+         res.status(200).send("Booked Successfully");
+    }else{
+         res.status(404).send("Please mention area")
+    }
+})
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
