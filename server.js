d11b8aac3c84ef48bb5adb05aa20beee737ed740
Premnath S <premsundarrajan4@gmail.com>
2025-11-25 11:37:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.post("/salesiq/webhook", (req, res) => {
     const body = req.body;
-
+    console.log(body);
     if(!body || !body.message){
         return res.status(400).json({ text: "Invalid request" });
     }
